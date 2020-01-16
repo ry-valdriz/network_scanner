fef645154705048de5819132de3f9b96212f459d
ry-valdriz <ryvaldriz101@gmail.com>
2020-01-16 17:49:51 -0500

    scp

--- network_scanner.py
+++ network_scanner.py
@@ -4,6 +4,10 @@ import scapy.all as scp
 
 
 def scan(ip):
-    scp.arping(ip)
+    # scp.arping(ip)
+    arp_request = scp.ARP()
+    arp_request.pdst = ip
+    print(arp_request.summary())
+    scp.ls(scp.ARP())
 
 scan("10.0.2.1/24")
